fbf9c619944a43b13741c1323efef4ce11456c18
Robert Rossmann <rr.rossmann@me.com>
2016-04-18 13:47:32 +0200

    Update eslint config files & style

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+/**
+ * sails-hook-events
+ *
+ * @author     Robert Rossmann <rr.rossmann@me.com>
+ * @copyright  2015 Robert Rossmann
+ * @license    http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
+ */
+
+'use strict'
+
+module.exports = {
+  extends: [
+    '@strv/javascript/environments/nodejs/latest',
+    '@strv/javascript/environments/nodejs/best-practices',
+    '@strv/javascript/environments/nodejs/optional',
+    '@strv/javascript/coding-styles/base'
+  ]
+}
---
+++ test/.eslintrc.js
@@ -0,0 +1,19 @@
+/**
+ * sails-hook-events
+ *
+ * @author     Robert Rossmann <rr.rossmann@me.com>
+ * @copyright  2015 Robert Rossmann
+ * @license    http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
+ */
+
+'use strict'
+
+module.exports = {
+  env: {
+    mocha: true
+  },
+
+  globals: {
+    sails: true
+  }
+}
