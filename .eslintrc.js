4346737f2289c572f1882c9c1d32c35d2db3d903
Robert Rossmann <rr.rossmann@me.com>
2016-08-20 12:49:00 +0200

    Upgrade deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@
 
 module.exports = {
   extends: [
-    '@strv/javascript/environments/nodejs/latest',
+    '@strv/javascript/environments/nodejs/v5',
     '@strv/javascript/environments/nodejs/best-practices',
     '@strv/javascript/environments/nodejs/optional',
     '@strv/javascript/coding-styles/base'
